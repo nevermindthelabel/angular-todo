d81e65f923217d0c01bee342e178ad42ec67612e
Matt Kilcup <nevermindthelabel@gmail.com>
2020-01-22 18:05:48 -0700

    added todos component

---
+++ angular-todos/src/app/components/todos/todos.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../models/Todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.scss']
+})
+export class TodosComponent implements OnInit {
+  todos: Todo[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.todos = [
+      {
+        id: 1,
+        title: 'todo 1',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'todo 2',
+        completed: true
+      },
+      {
+        id: 3,
+        title: 'todo 3',
+        completed: false
+      },
+      {
+        id: 4,
+        title: 'todo 4',
+        completed: false
+      }
+    ]
+  }
+
+}
