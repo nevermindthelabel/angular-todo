9920a0c0e2fb0bc26c18f35679cea8a6ff13d555
Matt Kilcup <nevermindthelabel@gmail.com>
2020-01-23 22:05:56 -0700

    added http module

---
+++ angular-todos/src/app/services/todo.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClientModule, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+
+  constructor() { }
+
+  getTodos() {
+    return [
+      {
+        id: 1,
+        title: 'todo 1',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'todo 2',
+        completed: true
+      },
+      {
+        id: 3,
+        title: 'todo 3',
+        completed: false
+      },
+      {
+        id: 4,
+        title: 'todo 4',
+        completed: false
+      }
+    ]
+  }
+}
