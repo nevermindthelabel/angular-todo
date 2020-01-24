e8ac04c6004fafb19f6c355ec01a1f6686f30b89
Matt Kilcup <nevermindthelabel@gmail.com>
2020-01-23 22:21:57 -0700

    added observable

--- angular-todos/src/app/services/todo.service.ts
+++ angular-todos/src/app/services/todo.service.ts
@@ -1,35 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Todo } from '../models/Todo';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  todosURL: string = 'https://jsonplaceholder.typicode.com/todos';
 
   constructor(private http: HttpClient) { }
 
-  getTodos() {
-    return [
-      {
-        id: 1,
-        title: 'todo 1',
-        completed: false
-      },
-      {
-        id: 2,
-        title: 'todo 2',
-        completed: true
-      },
-      {
-        id: 3,
-        title: 'todo 3',
-        completed: false
-      },
-      {
-        id: 4,
-        title: 'todo 4',
-        completed: false
-      }
-    ]
+  getTodos(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(this.todosURL);
   }
 }
