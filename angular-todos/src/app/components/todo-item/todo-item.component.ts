88b30df771a5ed8a835015fc1469c1ce9ec68030
Matt Kilcup <nevermindthelabel@gmail.com>
2020-01-24 20:45:35 -0700

    added todo service and ontoggle method

--- angular-todos/src/app/components/todo-item/todo-item.component.ts
+++ angular-todos/src/app/components/todo-item/todo-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Todo } from 'src/app/models/Todo';
+import { TodoService } from '../../services/todo.service';
 
 @Component({
   selector: 'app-todo-item',
@@ -9,7 +10,7 @@ import { Todo } from 'src/app/models/Todo';
 export class TodoItemComponent implements OnInit {
   @Input() todo: Todo;
 
-  constructor() { }
+  constructor(private todoService: TodoService) { }
 
   ngOnInit() {
   }
@@ -24,7 +25,10 @@ export class TodoItemComponent implements OnInit {
   }
 
   onToggle(todo) {
+    //toggle UI
     todo.completed = !todo.completed;
+    // toggle data on mock server
+    this.todoService.toggleCompleted(todo).subscribe(todo => console.log(todo));
   }
 
   onDelete(todo) {
