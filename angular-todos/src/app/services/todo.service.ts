dd5afe4f4b8779072e4052e3a546b52b7142e0b5
Matt Kilcup <nevermindthelabel@gmail.com>
2020-01-24 20:49:49 -0700

    added put request to update data on mock server

--- angular-todos/src/app/services/todo.service.ts
+++ angular-todos/src/app/services/todo.service.ts
@@ -3,15 +3,27 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Todo } from '../models/Todo';
 import { Observable } from 'rxjs';
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
   todosURL: string = 'https://jsonplaceholder.typicode.com/todos';
+  todosLimit: string = '?_limit=5';
 
   constructor(private http: HttpClient) { }
 
+  // Get todos from json placeholder
   getTodos(): Observable<Todo[]> {
-    return this.http.get<Todo[]>(this.todosURL);
+    return this.http.get<Todo[]>(`${this.todosURL}${this.todosLimit}`);
+  }
+  // Toggle completed on mock server
+  toggleCompleted(todo: Todo): Observable<any> {
+    const url = `${this.todosURL}/${todo.id}`
+    return this.http.put(url, todo, httpOptions)
   }
 }
